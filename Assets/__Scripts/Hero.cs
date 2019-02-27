502565130e62c17d0131a3c7772fe8d7f4415939
Femi Adebowale <feadebowale@gmail.com>
2019-02-27 00:34:26 -0500

    Hero Script + Shield Addition

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -2,15 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hero : MonoBehaviour {
+public class Hero : MonoBehaviour
+{
+    static public Hero S; //Singleton
+    [Header("Set in Inspector")]
+    //These fields control movement of ship
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [Header("Set Dynamically")]
+    public float shieldLevel = 1;
+
+    void Awake()
+    {
+        if(S == null)
+        {
+            S = this; //Set the singleton
+        } else
+        {
+            Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        //Change transform.position based on axis
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        //rotate the ship to make it feel more dynamic
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+
+    }
 }
